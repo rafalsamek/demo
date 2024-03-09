51dd243c10ae33aa90e621ce945e41989cbce5d6
rsamek <rafal.samek@gmail.com>
2024-03-09 15:30:20 +0100

    Lazy

--- src/main/java/com/example/demo/Engine.java
+++ src/main/java/com/example/demo/Engine.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Engine {
+    public Engine(@Lazy Car car) {
+
+    }
 }
