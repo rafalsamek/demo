6268e0216912a0e307b74ecc28b2cbf3e868ef05
rsamek <rafal.samek@gmail.com>
2024-03-09 11:20:16 +0100

    CommandLineRunner with Logger

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		LOGGER.info("Application Stop");
+		System.out.println("Application Stop");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		LOGGER.info("Hello World!");
+	}
 }
