fccb373e94cb2e7ed558179e15cb126b302b6c92
rsamek <rafal.samek@gmail.com>
2024-03-09 12:41:36 +0100

    Dependency Infection: Car and Engine with Autowired

--- src/main/java/com/example/demo/Car.java
+++ src/main/java/com/example/demo/Car.java
@@ -1,15 +1,17 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Car {
     private Engine engine;
 
-    public Car(Engine engine) {
-        this.engine = engine;
-    }
+//    public Car(Engine engine) {
+//        this.engine = engine;
+//    }
 
+    @Autowired
     public void setEngine(Engine engine) {
         this.engine = engine;
     }
