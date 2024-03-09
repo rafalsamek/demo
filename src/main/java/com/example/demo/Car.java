601c878af653463204d54d3e8e4068f2c206ddaa
rsamek <rafal.samek@gmail.com>
2024-03-09 12:29:56 +0100

    Dependency Infection: Car and Engine

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class Car {
+    private Engine engine;
+
+    public Car(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+}
